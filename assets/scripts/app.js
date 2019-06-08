d6f2683d952c5fb1a8537c7c38b574cea4a06125
DaveOrDavid <david.santore@gmail.com>
2019-06-08 07:56:27 -0400

    UI tracker set up, Auth API connect works
    Created event listeners, tested API auth with sign in successful

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,12 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events')
 
 $(() => {
-  // your JS code goes here
+  // authentication events on client side
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#sign-out').on('submit', authEvents.onSignOut)
+  $('#change-pw').on('submit', authEvents.onChangePassword)
 })
